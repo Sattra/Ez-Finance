a23caf7d0422d11949b09e1eab47547428eb1d9c
Illya <s7070111@kmitl.ac.th>
2014-12-06 09:38:37 +0700

    table1to4

---
+++ table1to4.py
@@ -0,0 +1,16 @@
+def tablea_1(k, n, money):
+    values = (1+k)**n
+    return values*money
+
+def table_2(k, n, money):
+    values = (((1+k)**n)-1)/k
+    return values*money
+
+def table_3(k, n):
+    values = 1/((1+k)**n)
+    return values
+
+def table_4(k, n, money):
+    values = (1-(1/((1+k)**n)))/k
+    return values*money
+
