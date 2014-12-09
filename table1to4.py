71aea8217948cd43c4c6813aa7ad1a4c3d02a442
Illya <s7070111@kmitl.ac.th>
2014-12-09 10:47:44 +0700

    change interest * 1/100 and int(money)

--- table1to4.py
+++ table1to4.py
@@ -1,24 +1,28 @@
 def table_1(interest, year, money):
+    money = int(money)
     n = int(year)
-    k = float(interest)
+    k = (float(interest))/100
     values = ((1+k)**n)*money
     return values
 
 def table_2(interest, year, money):
+    money = int(money)
     n = int(year)
-    k = float(interest)
+    k = (float(interest))/100
     values = ((((1+k)**n)-1)/k)*money
     return values
 
 def table_3(interest, year, money):
+    money = int(money)
     n = int(year)
-    k = float(interest)
+    k = (float(interest))/100
     values = (1/((1+k)**n))*money
     return values
 
 def table_4(interest, year, money):
+    money = int(money)
     n = int(year)
-    k = float(interest)
+    k = (float(interest))/100
     values = ((1-(1/((1+k)**n)))/k)*money
     return values
 
